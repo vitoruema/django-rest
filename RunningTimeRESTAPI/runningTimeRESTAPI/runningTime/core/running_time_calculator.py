713152a3cc38bc7ce16811d19076baee845fb669
vitoruema <vitor.uema@railmp.com>
2020-03-19 14:54:27 -0300

    adicao de um teste melhor e correcao do erro no calculo da velocidade inicial

--- RunningTimeRESTAPI/runningTimeRESTAPI/runningTime/core/running_time_calculator.py
+++ RunningTimeRESTAPI/runningTimeRESTAPI/runningTime/core/running_time_calculator.py
@@ -10,27 +10,30 @@ def track_running_time(track, pred_ini, train_type, length, segment_origin):
     x[0] = pred_ini
     x[1] = length
 
+    print(segment_origin)
+    print(x)
+
     load_model = pickle.load(open("runningTime/core/models/{}.sav".format(track), "rb"))
-    #print(load_model.predict(np.array(x).reshape(1, -1)))
 
     return {track: load_model.predict(np.array(x).reshape(1, -1))[0]}
 
 
-def running_time(train_type, length, segments, track_length, seg_ini_name, pred_ini):
-    pred_media = 0
-    running_time_dict = {}
+def running_time(train_type, length, segments, track_length, seg_ini_name, velo_ini):
     if not segments:
         return {}
     else:
+        velo_media = 0
+        running_time_dict = {}
+
         seg_head, *seg_tail = segments
-        seg_name, tracks = seg_head
+        [seg_name], tracks = seg_head
+
         for track in tracks:
-            running_time_dict = track_running_time(track, pred_ini, train_type, length, seg_ini_name)
-            pred_media += running_time_dict[track]
+            running_time_dict = {**running_time_dict,
+                                 **track_running_time(track, track_length[track] / velo_ini,
+                                                      train_type, length, seg_ini_name)}
+            velo_media += track_length[track] / running_time_dict[track]
 
-        pred_media = pred_media / len(tracks)
+        velo_media = velo_media / len(tracks)
 
-        return {**running_time_dict, **running_time(train_type, length, seg_tail, track_length, seg_name, pred_media)}
-
-
-# track_running_time("CL10.81508", 20, "TRAIN_TYPE_ID_BOre", 2360, "SEGMENT_ORIGIN_CL52BT")
+        return {**running_time_dict, **running_time(train_type, length, seg_tail, track_length, seg_name, velo_media)}
--- RunningTimeRESTAPI/runningTimeRESTAPI/runningTime/views.py
+++ RunningTimeRESTAPI/runningTimeRESTAPI/runningTime/views.py
@@ -8,34 +8,15 @@ from .core.running_time_calculator import running_time
 
 @api_view(['GET', 'POST'])
 def get_running_time(request, format=None):
-    """
-    List all code running_time, or create a new snippet.
-    """
     if request.method == 'GET':
         serializer = TrainInfoSerializer(data=request.data)
-        # print(serializer)
         if serializer.is_valid():
             d = serializer.data
-            print(running_time(d["train_type"], d["train_length"], d["segments"],
-                               d["track_length"], d["segments"][0][0][0], d["pred_ini"]))
 
             x = RunningTime(running_time=running_time(d["train_type"], d["train_length"], d["segments"],
                                                       d["track_length"], d["segments"][0][0][0], d["pred_ini"]))
             resp = RunningTimeSerializer(x)
-            print(x)
 
             return Response(resp.data, status=status.HTTP_201_CREATED)
-        #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        # running_time = Snippet.objects.all()
-        # serializer = SnippetSerializer(running_time, many=True)
-        # return Response(serializer.data)
 
-    elif request.method == 'POST':
-        print("ok")
-        serializer = TrainInfoSerializer(data=request.data)
-
-        if serializer.is_valid():
-            print(serializer.data)
-            # serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
